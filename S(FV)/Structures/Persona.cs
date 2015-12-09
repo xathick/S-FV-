63406cbdda68e7a213ac03d863cba9436c500a08
Xathick <xathick@gmail.com>
2015-12-09 11:51:29 +0100

    Initial commir

---
+++ S(FV)/Structures/Persona.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_FV_.Structures
+{
+    class Persona
+    {
+        #region Parametros
+        String nombre;
+        String apellido1;
+        String apellido2;
+        String telefono;
+        String email;
+        DateTime fechaNacimiento;
+        #endregion
+
+        #region Encapsulated Fields
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellido1
+        {
+            get
+            {
+                return apellido1;
+            }
+
+            set
+            {
+                apellido1 = value;
+            }
+        }
+
+        public string Apellido2
+        {
+            get
+            {
+                return apellido2;
+            }
+
+            set
+            {
+                apellido2 = value;
+            }
+        }
+
+        public string Telefono
+        {
+            get
+            {
+                return telefono;
+            }
+
+            set
+            {
+                telefono = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get
+            {
+                return fechaNacimiento;
+            }
+
+            set
+            {
+                fechaNacimiento = value;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Constructor vacio de un objeto Persona, inicializando todos los componentes a NULL
+        /// </summary>
+        public Persona()
+        {
+            this.nombre = null;
+            this.apellido1 = null;
+            this.apellido2 = null;
+            this.telefono = null;
+            this.email = null;
+            this.fechaNacimiento = new DateTime(1,1,1);
+        }
+
+        /// <summary>
+        /// Genera un objeto Persona con parámetros
+        /// </summary>
+        /// <param name="nombre">Primer apellido de la persona</param>
+        /// <param name="apellido2">Segundo apellido de la persona</param>
+        /// <param name="telefono">Telefono de la persona</param>
+        /// <param name="email">Email de la persona</param>
+        /// <param name="fechaNacimiento">Fecha de nacimiento de la persona</param>
+        public Persona(String nombre, String apellido1, String apellido2, String telefono, String email, DateTime fechaNacimiento)
+        {
+            this.nombre = nombre;
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            this.telefono = telefono;
+            this.email = email;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        /// <summary>
+        /// Crea un DateTime con los parametros pasados
+        /// </summary>
+        /// <param name="dia">Día - numérico</param>
+        /// <param name="mes">Mes - numérico</param>
+        /// <param name="año">Año - numérico</param>
+        /// <returns></returns>
+        public DateTime CrearFecha(int dia, int mes, int año)
+        {
+            DateTime fecha = new DateTime(año,mes,dia);
+            return fecha;
+        }
+
+
+    }
+}
---
+++ S(FV)/Structures/User.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_FV_
+{
+    class User
+    {
+        #region Parametros
+        String nombre;
+        String pass;
+        String idParroquia;
+        String idPersona;
+        #endregion
+
+        #region Encapsulated Fields
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Pass
+        {
+            get
+            {
+                return pass;
+            }
+
+            set
+            {
+                pass = value;
+            }
+        }
+
+        public string IdPersona
+        {
+            get
+            {
+                return idPersona;
+            }
+
+            set
+            {
+                idPersona = value;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Constructor que inicializa a NULL todos los elementos y genera un objeto User
+        /// </summary>
+        public User()
+        {
+            this.nombre = null;
+            this.pass = null;
+            this.idPersona = null;
+        }
+
+        /// <summary>
+        /// Constructor que inicializa con los parametros facilitados todos los elemnetos, generando un objeto User con parámetros
+        /// </summary>
+        /// <param name="nombre">Nombre del usuario</param>
+        /// <param name="pass">Contraseña del usuario</param>
+        /// <param name="idPersona">Vinculo con el objeto Persona correspondiente</param>
+        public User(String nombre, String pass, String idPersona)
+        {
+            this.nombre = nombre;
+            this.pass = pass;
+            this.idPersona = idPersona;
+        }
+
+    }
+}

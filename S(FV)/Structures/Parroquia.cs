5b1d6ebe6d8e5771e3a7d9635b4816a250800cce
Xathick <xathick@gmail.com>
2015-12-09 12:07:59 +0100

    -Modified Parroquia

---
+++ S(FV)/Structures/Parroquia.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_FV_.Structures
+{
+    class Parroquia
+    {
+        #region Parametros
+        String idParroquia;
+        String nombre;
+        #endregion
+
+        #region Encapsulated Fields
+        public string IdParroquia
+        {
+            get
+            {
+                return idParroquia;
+            }
+
+            set
+            {
+                idParroquia = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+        #endregion
+       
+        /// <summary>
+        /// Genera un objeto tipo Parroquia inicializado a NULL
+        /// </summary>
+        public Parroquia()
+        {
+            this.idParroquia = null;
+            this.nombre = null;
+        }
+
+        /// <summary>
+        /// Genera un objeto Parroquia inicializado con los parametros especificados
+        /// </summary>
+        /// <param name="idParroquia">id</param>
+        /// <param name="nombre"></param>
+        public Parroquia(String idParroquia, String nombre)
+        {
+            this.nombre = nombre;
+            this.idParroquia = nombre;
+        }
+    }
+}
